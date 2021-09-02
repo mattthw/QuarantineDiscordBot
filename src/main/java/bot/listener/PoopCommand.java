86c7a5be8cef391fc23a5fb1be8021789d33f7cd
Matt <imattxc@gmail.com>
2021-09-02 01:44:07 -0700

    attach poop command to main app

--- src/main/java/bot/listener/PoopCommand.java
+++ src/main/java/bot/listener/PoopCommand.java
@@ -11,29 +11,26 @@ import java.util.regex.Pattern;
 public class PoopCommand extends ListenerAdapter {
     @Override
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
-        super.onGuildMessageReceived(event);
+        poopOnFriendsChance(event);
     }
 
-    private void poopOnFriendsChance(GuildMessageEvent m) {
-        String input = m.getMessage().getContentRaw();
+    private void poopOnFriendsChance(GuildMessageReceivedEvent event) {
+        String input = event.getMessage().getContentRaw();
         String[] args = input.split(" ");
-        boolean commandMatch = Pattern.compile(Pattern.quote("!poop"), Pattern.CASE_INSENSITIVE).matcher(input).find();
+        boolean commandMatch = Pattern.compile(Pattern.quote("!poop"), Pattern.CASE_INSENSITIVE).matcher(args[0]).find();
         if (commandMatch) {
             double chance = Math.random();
             double threshold = 0.01;
-            if (args.length == 2) {
+            if (args[1] != null) {
                 threshold = Double.parseDouble(args[1]);
+                log.info(String.format("using custom threshold: %s", threshold));
             }
             if (chance <= threshold) {
-                log.info(String.format("%n%s rolled a %.2f out of %.2f, I am gonna poop all over him", m.getUsername(), chance, threshold));
-                m.getMessage().addReaction("💩").queue();
-                m.getMessage().addReaction("🥳").queue();
+                log.info(String.format("%n%s rolled a %.2f out of %.2f, I am gonna poop all over him", event.getMessage().getAuthor().getName(), chance, threshold));
+                event.getMessage().addReaction("💩").queue();
             } else {
-                log.info(String.format("%n%s rolled a %.2f out of %.2f, I will not poop on him", m.getUsername(), chance, threshold));
-                m.getMessage().addReaction("\uD83C\uDDEB").queue(); // F
-                m.getMessage().addReaction("\uD83C\uDDE6").queue(); // A
-                m.getMessage().addReaction("\uD83C\uDDEE").queue(); // I
-                m.getMessage().addReaction("\uD83C\uDDF1").queue(); // L
+                log.info(String.format("%n%s rolled a %.2f out of %.2f, I will not poop on him", event.getMessage().getAuthor().getName(), chance, threshold));
+                event.getMessage().addReaction("👎").queue();
             }
         }
     }
